2fa6ffbf1d03473345ddc55ad8a0b50847673eb7
Dominic Chuma <chumadominic@gmail.com>
2022-08-19 17:02:02 +0100

    Some style edits 2

--- src/App.js
+++ src/App.js
@@ -29,34 +29,34 @@ function App() {
         </a>
       </header> */}
       
-      <Container> {/*Remember to inline style the container (Top & Buttom margin)*/}
+      <Container className='Container'> {/*Remember to inline style the container (Top & Buttom margin)*/}
         {/* <Row xs={1} md={2} className="g-4">  */}
         {/* <Row style={{textAlign: "center"}}> */}
-        <Row className='justify-content-md-center text-center'  style={{textAlign: "center"}}> {/*Inline style Row (Height, Top/bottom Margin, Padding{Where necessary})*/}
-          <Col md='auto' >
-            <h1>Pick your Game</h1> {/*Inline style the font*/}
-          </Col>
-        </Row>
-        <Row className="g-5">
-          {Array.from({ length: 5 }).map((_, idx) => (
-            <Col sm='6' md='4' lg='4'>
-              <Card border='light' className='Card'>
-              {console.log(idx)} 
-                <Card.Img variant="top" src={"Pictures/logo" + ++idx + ".png"} className='CardImage' /> {/*Inline Style tis for Min & Max Height*/}
-                  <Card.Body>
-                    {/* <Card.Title>Card title !</Card.Title> */}
-                    {/* <Card.Text>
-                      This is a longer card with supporting text below as a natural
-                      lead-in to additional content. This content is a little bit
-                      longer.
-                    </Card.Text> */}
-                   <Card.Footer className="text-muted text-center text-sm-center CardFooter">{gameName[--idx]}</Card.Footer> {/*Use text-center intead of text-sm-center*/}
-                </Card.Body>
-              </Card>
+          <Row className='justify-content-md-center text-center'  style={{textAlign: "center"}}> {/*Inline style Row (Height, Top/bottom Margin, Padding{Where necessary})*/}
+            <Col md='auto' >
+              <h1>Pick your Game</h1> {/*Inline style the font*/}
             </Col>
-          
-          ))}
-        </Row>
+          </Row>
+          <Row className="g-5">
+            {Array.from({ length: 5 }).map((_, idx) => (
+              <Col sm='6' md='4' lg='4'>
+                <Card border='light' className='Card'>
+                {console.log(idx)} 
+                  <Card.Img variant="top" src={"Pictures/logo" + ++idx + ".png"} className='CardImage' /> {/*Inline Style tis for Min & Max Height*/}
+                    <Card.Body>
+                      {/* <Card.Title>Card title !</Card.Title> */}
+                      {/* <Card.Text>
+                        This is a longer card with supporting text below as a natural
+                        lead-in to additional content. This content is a little bit
+                        longer.
+                      </Card.Text> */}
+                    <Card.Footer className="text-muted text-center text-sm-center CardFooter">{gameName[--idx]}</Card.Footer> {/*Use text-center intead of text-sm-center*/}
+                  </Card.Body>
+                </Card>
+              </Col>
+            
+            ))}
+          </Row>
       </Container>
     </div>
   );
