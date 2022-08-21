17ca7fb982292649e1b24676a462af80c6538854
Dominic Chuma <chumadominic@gmail.com>
2022-08-21 19:04:07 +0100

    Function 1 finally written

--- src/function1.js
+++ src/function1.js
@@ -1,13 +1,13 @@
-let multiple = function (number) {
+let multiple = function (number, total) {
     // console.log("I hate y'all");
-    var numb1 = 31; // Total games recorded
+    var numb1 = total; // Total games recorded
     var numb2 = 6; // The number of games for each type/category of games
     var numb3 = Math.trunc(numb1 / numb2) ; // Number of game categories/type
     var numb4 = 1; // Start number...
     var numb5 ; // End number....
 
     console.log(numb3);
-    if(number <= numb3 && numb1 % numb2 === 0){
+    if(number <= numb3 && numb1 % numb2 === 0){ // If the No. of records are even.
         // Do this
         if (number === 1){
             numb4 = 1;
@@ -25,17 +25,41 @@ let multiple = function (number) {
                 // return [numb4,numb5];
             }
         }
-    }else{
+    }else{ // If the total number of records are Odd
         //; // Do nothing....{Empty Statement}
         console.log('Na lie o!!');
         if (number <= numb3 && numb1 % numb2 !== 0){ // If there's a remainder
-            if (numb1 % numb2 === 1){ // If remainder is 1
-                return [];
+            if (number === 1){
+                numb4 = 1;
+                numb5 = numb2;
+                console.log(numb4, numb5);
+                return [numb4, numb5];
+            }else{
+                if (number !== 0 && number > 1){
+                    numb4 = 1 + (numb2 * (number - 1));
+                    numb5 = numb2 + (numb2 * (number - 1));
+                    console.log(numb4, numb5);
+                    return [numb4, numb5];
+                    // return [numb4,numb5];
+                }
             }
-            // return []
+    
+        }else if ((number > numb3) && (number - numb3 === 1) && (numb1 % numb2 !== 0)){
+
+            if (numb1 % numb2 === 1){ // If remainder is 1
+                numb4 = numb1;
+                console.log(numb4);
+                return [numb4];
+            }
+            else if (numb1 % numb2 > 1){
+                numb4 = 1 + (numb2 * (number - 1));
+                numb5 = (numb1 % numb2) + (numb2 * (number - 1))
+                console.log(numb4, numb5);
+                return [numb4, numb5];
+
+            }
         }
     }
-
 }
 
-multiple(5);
+multiple(5,30);
